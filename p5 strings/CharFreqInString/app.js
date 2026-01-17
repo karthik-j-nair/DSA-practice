bf3898ae8c0c64eadaf3cd32f26d175a3151ffcf
Karthik J Nair <you@example.com>
2026-01-17 19:43:18 +0530

    chech frequency of chat in string

---
+++ p5 strings/CharFreqInString/app.js
@@ -0,0 +1,13 @@
+let str = "karthik";
+let arr = new Array(128).fill(0);
+
+for(let i = 0; i<str.length; i++){
+    let ascii = str.charCodeAt(i);
+    arr[ascii] = arr[ascii] + 1;
+}
+
+for(let i = 0; i<arr.length; i++){
+    if (arr[i]>0) {
+        console.log(`${String.fromCharCode(i)} - ${arr[i]}`);
+    }
+}
