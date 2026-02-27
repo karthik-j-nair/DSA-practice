9aac83a7445f979a0c7b044e8775b11c6e11eb16
Karthik J Nair <you@example.com>
2026-02-27 17:26:01 +0530

    remove duplicates from a sorted array in-place

---
+++ p4 arrays/10 removeDuplicateInSorted.js
@@ -0,0 +1,18 @@
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+// Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+
+// The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+
+let nums = [0,0,1,1,1,2,2,3,3,4];
+
+let i = 0, j = i+1;
+
+while (i < nums.length - 1) {
+    if(nums[i] !== nums[i+1]){
+        nums[j++] = nums[i+1]
+    }
+    i++
+}
+
+console.log(nums);
